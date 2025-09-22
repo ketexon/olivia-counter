ef8b31f603d7722709f97667df15186dd9c50b6c
ketexon <zane.a.s.clark@gmail.com>
2025-09-22 14:02:08 -0700

    removed counters

--- index.js
+++ index.js
@@ -1,47 +1,6 @@
-import { cachedCounts, getCounts, incrementCount } from "./counts.js";
 import { spawnEmoji } from "./emoji.js";
 import { getTodaysToneIndicator } from "./tone-indicators.js";
 import "./dates.js";
-
-document.querySelectorAll(".counter")
-	.forEach(
-		/**
-		* @param {HTMLElement} counter
-		*/
-		async (counter) => {
-			const counterName = counter.dataset.counterName;
-			if (!counterName) {
-				console.warn("Counter name not specified attribute.");
-				return;
-			}
-
-			/** @type {HTMLElement} */
-			const text = counter.querySelector(".counter__text");
-			// populate counts from cache if available
-			if (cachedCounts) {
-				const count = cachedCounts.get(counterName);
-				if (count !== undefined) {
-					text.textContent = count;
-				}
-			}
-
-			// populate initial count
-			const counts = await getCounts();
-			const count = counts.get(counterName);
-			text.textContent = count !== undefined ? count : "0";
-
-			// update count on click
-			/** @type {HTMLButtonElement} */
-			const incrementButton = counter.querySelector(".counter__increment-button");
-			incrementButton.addEventListener("click", async (ev) => {
-				const newCount = counts.get(counterName) + 1;
-				counts.set(counterName, newCount);
-				text.textContent = newCount;
-				spawnEmoji(counterName, ev);
-				await incrementCount(counterName);
-			});
-		}
-	);
 
 const toneIndicator = getTodaysToneIndicator();
 document.querySelector(".tone-indicator__name").textContent = `${toneIndicator.indicator} ${toneIndicator.emoji}`;
